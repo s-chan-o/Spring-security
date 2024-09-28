7781ce47647af7150f6993f3687be40c1ea8043f
김승찬 <s24041@gsm.hs.kr>
2024-09-28 11:05:49 +0900

    security practice

--- src/main/java/com/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/testsecurity/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.testsecurity.config;
 
+import org.apache.catalina.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
@@ -23,27 +25,27 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/","/login", "/loginProc", "/join", "/joinProc").permitAll()
+                        .requestMatchers("/", "/login", "/loginProc", "/join", "/joinProc").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/my/**").hasAnyRole("ADMIN","USER")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
                 );
 
         http
                 .formLogin((auth) -> auth.loginPage("/login")
-                    .loginProcessingUrl("/loginProc")
+                        .loginProcessingUrl("/loginProc")
                         .permitAll()
                 );
 
-        //http
-        //        .csrf((auth) -> auth.disable());
+        http
+                .csrf((auth) -> auth.disable());
 
         http
                 .sessionManagement((auth) -> auth
-                .maximumSessions(1)
+                        .maximumSessions(1)
                         .maxSessionsPreventsLogin(true)
 
-        );
+                );
 
         http
                 .sessionManagement((auth) -> auth
