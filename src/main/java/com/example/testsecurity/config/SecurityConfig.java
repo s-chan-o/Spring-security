944f701edae61874904f16bf28b03d222d9341df
김승찬 <s24041@gsm.hs.kr>
2024-09-28 11:08:04 +0900

    security practice

--- src/main/java/com/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/testsecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.testsecurity.config;
 import org.apache.catalina.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,10 +33,7 @@ public class SecurityConfig {
                 );
 
         http
-                .formLogin((auth) -> auth.loginPage("/login")
-                        .loginProcessingUrl("/loginProc")
-                        .permitAll()
-                );
+                .httpBasic(Customizer.withDefaults());
 
         http
                 .csrf((auth) -> auth.disable());
